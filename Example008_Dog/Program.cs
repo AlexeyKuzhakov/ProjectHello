fb20e1aee37e254af4ca5cf06aa9cb64acfc26ec
Alexey <kuzhakov.fx@gmail.com>
2022-07-08 11:53:14 +0300

    в example008 dog добавил ввод входных данных

--- Example008_Dog/Program.cs
+++ Example008_Dog/Program.cs
@@ -1,14 +1,23 @@
-﻿int a = 1; //first friend speed
-int b = 2; //second friend speed
-int d = 5; // dog spees
+﻿Console.Write("Введите скорость первого друга ");
+int a = Convert.ToInt32(Console.ReadLine()); // Скорость первого друга
+
+Console.Write("Введите скорость второго друга ");
+int b = Convert.ToInt32(Console.ReadLine()); // Скорость второго друга
+
+Console.Write("Введите скорость собаки ");
+int d = Convert.ToInt32(Console.ReadLine()); // Скорость собаки
+
+Console.Write("На каком расстоянии друзья встретятся?  ");
+int x = Convert.ToInt32(Console.ReadLine()); // Расстояние на котором закончить циклсд
+
 int friend = 2;
 int count = 0;
 int time = 0;
 
 Console.Write("Введите дистанцию ");
-int distance = Convert.ToInt32(Console.ReadLine());
+int distance = Convert.ToInt32(Console.ReadLine()); //Дистанция между друзьями
 
-while(distance > 10)
+while(distance > x)
 {
     if (friend == 1)
     {
