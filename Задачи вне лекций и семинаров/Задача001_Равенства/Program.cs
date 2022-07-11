9988e7e01fd68eeafa05f7cc7a417a6bf268452d
Alexey <kuzhakov.fx@gmail.com>
2022-07-11 15:14:50 +0300

    создал папку вне лекций, сделал первую задачу на равенство

---
+++ Задачи вне лекций и семинаров/Задача001_Равенства/Program.cs
@@ -0,0 +1,22 @@
+﻿/*
+Консольная программа, где пользователь будет вводить 2 числа. 
+Вывести равны ли числа, и если нет, то какое число больше
+*/
+
+Console.WriteLine("Введите первое число: ");
+int a = int .Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int b = int .Parse(Console.ReadLine());
+
+if(a == b)
+{
+    Console.WriteLine("Числа равны");
+}
+else if(a > b)
+{
+    Console.WriteLine(a + " больше чем " + b);
+}
+else
+{
+    Console.WriteLine(b + " больше чем " + a);
+}
