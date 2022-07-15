965e2c95f9edaa91b3c7880091806e7b703a2591
Alexey <kuzhakov.fx@gmail.com>
2022-07-15 21:30:16 +0300

    Добавил вторую задачу с третьего семинара

---
+++ Семинар 3/Задачи с семинара/Задача02/Program.cs
@@ -0,0 +1,30 @@
+﻿/*
+Напишите программу, которая по заданному номеру четверти, 
+показывает диапазон возможных координат точек в этой четверти (x и y).
+*/
+
+Console.Write("Введите номер четверти: ");
+int num = int.Parse(Console.ReadLine());
+Console.WriteLine("Ответ: " + foundXY(num));
+string foundXY (int a)
+{
+ if(a == 1)
+ {
+    return "x > 0 & y > 0";
+ }
+ if(a == 2)
+ {
+    return "x < 0 & y > 0";
+ }
+ 
+ if(a == 3)
+ {
+    return "x < 0 & y < 0";
+ }
+ 
+ if(a == 4)
+ {
+    return "x > 0 & y < 0";
+ }
+ return "Ошибка. Введите от 1 до 4";
+}
