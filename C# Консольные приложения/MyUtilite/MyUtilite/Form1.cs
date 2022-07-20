4e222f04a313670e8125ad7be409a1ee38f4b5f6
Alexey <kuzhakov.fx@gmail.com>
2022-07-20 21:46:57 +0300

    добавил меню в проект первого оконного приложения по курсу c# мини-программы

--- C# Консольные приложения/MyUtilite/MyUtilite/Form1.cs
+++ C# Консольные приложения/MyUtilite/MyUtilite/Form1.cs
@@ -10,11 +10,21 @@ using System.Windows.Forms;
 
 namespace MyUtilite
 {
-    public partial class Form1 : Form
+    public partial class MyForm : Form
     {
-        public Form1()
+        public MyForm()
         {
             InitializeComponent();
+        }
+
+        private void tsmiExit_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void tsmiAbout_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Программа мои утилиты, содержит ряд небольших программ, которые могут пригодиться в жизни. А главное научить меня основам программирования на C#. \nАвтор: Кужаков А.А.", "О программе");
         }
     }
 }
