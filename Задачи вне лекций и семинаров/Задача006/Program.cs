63b477190c6c55c6fee6f44875433297e79d6604
Alexey <kuzhakov.fx@gmail.com>
2022-07-26 20:41:42 +0300

    добавил шестую задачу вне лекций, с сортировкой и разворотом массива

---
+++ Задачи вне лекций и семинаров/Задача006/Program.cs
@@ -0,0 +1,12 @@
+﻿//Вывести максимальное число из цифр введенного числа
+// 453 - 543
+// 3485 - 8543
+
+
+Console.WriteLine("Эта программа выведет максимальное число из цифр, введеного вами числа.\nВведите число: ");
+int num = int.Parse(Console.ReadLine());
+char[] arr = num.ToString().ToCharArray();
+Array.Sort(arr);
+Array.Reverse(arr);
+Console.WriteLine(arr);
+
