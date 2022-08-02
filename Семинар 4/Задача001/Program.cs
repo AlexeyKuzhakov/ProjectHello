bbed716a1855a5a3d54577c4d3f979a2776fe39a
Alexey <kuzhakov.fx@gmail.com>
2022-08-02 21:35:32 +0300

    добавил задачи четвертого семинара

---
+++ Семинар 4/Задача001/Program.cs
@@ -0,0 +1,11 @@
+﻿//Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+
+Console.WriteLine("ВВедите число");
+int a = int.Parse(Console.ReadLine());
+int numbersSum = 0;
+
+for(int i = 1; i <=a; i++)
+{
+    numbersSum = numbersSum + i;
+}
+Console.WriteLine(numbersSum);
---
+++ Семинар 4/Задача002/Program.cs
@@ -0,0 +1,16 @@
+﻿// Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+
+Console.WriteLine("Введите число: ");
+/*
+string num = Console.ReadLine();
+int len = num.Length;
+Console.WriteLine(len);
+*/
+int num = int.Parse(Console.ReadLine());
+int i = 0;
+while(num != 0 )
+{
+    num = num / 10;
+    i++;
+}
+Console.Write(i);
