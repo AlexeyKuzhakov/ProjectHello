ba5aa96f27ff581a040b7d89c8a7587a6de653cb
Alexey <kuzhakov.fx@gmail.com>
2022-08-06 17:48:22 +0300

    Добавил первую задачу дз по пятому семинару

---
+++ Семинар 5/Практические задания/Задача01/Program.cs
@@ -0,0 +1,34 @@
+﻿/*
+Задайте массив заполненный случайными положительными трёхзначными числами. Напишите программу, 
+которая покажет количество чётных чисел в массиве.
+[345, 897, 568, 234] -> 2
+*/
+
+int[] getArray(int n)
+{
+    int[] arr = new int[n];
+    Random rnd = new Random();
+
+    for (int i = 0; i < n; i++)
+    {
+        arr[i] = rnd.Next(100, 1000);
+    }
+    return arr;
+}
+
+Console.WriteLine("Введите количество эллементов массива: ");
+int lenght = int.Parse(Console.ReadLine());
+int[] arr = getArray(lenght);
+int chet = 0;
+
+for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+        if(arr[i] % 2 == 0)
+        {
+            chet++;
+        }
+    }
+
+Console.WriteLine();
+Console.WriteLine("Четных чисел в массиве: " + chet);
