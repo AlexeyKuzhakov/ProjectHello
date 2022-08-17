605dd2abd7cb683e55f81ea5aabd700f66e942a3
Alexey <kuzhakov.fx@gmail.com>
2022-08-17 10:58:26 +0300

    Добавил первую задачу по дз с седьмого семинара

---
+++ Семинар 7/Практические задания/Задача001/Program.cs
@@ -0,0 +1,36 @@
+﻿// Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+
+Console.WriteLine("Введите количество строк и столбцов через пробел: ");
+string[] input = Console.ReadLine().Split();
+int row = int.Parse(input[0]);
+int col = int.Parse(input[1]);
+double[,] numbers = GetArray(row, col);
+PrintArray(numbers);
+
+double[,] GetArray(int row, int col)
+{
+    double[,] array = new double[row, col];
+    Random random = new Random();
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            array[i, j] = Math.Round(random.Next(0, 1)+ random.NextDouble(),2);
+        }
+    }
+    return array;
+}
+
+
+void PrintArray(double[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write(array[i, j] + "\t");
+        }
+        Console.WriteLine();
+    }
+}
