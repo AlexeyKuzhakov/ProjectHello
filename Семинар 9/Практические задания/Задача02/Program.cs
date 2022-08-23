3108134639a8a33c5e6f461fe7d4e24f7d695076
Alexey <kuzhakov.fx@gmail.com>
2022-08-23 16:07:26 +0300

    Добавил вторую задачу дз с девятого семинара

---
+++ Семинар 9/Практические задания/Задача02/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+Задайте значения M и N. Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+M = 1; N = 15 -> 120
+M = 4; N = 8. -> 30
+*/
+
+Console.WriteLine("Эта программа выводит сумму натуральных чисел между заданными значениями.");
+Console.WriteLine("Введите начальное и последнее значения через пробел и нажмите enter: ");
+string[] input = Console.ReadLine().Split();
+int start = int.Parse(input[0]);
+int end = int.Parse(input[1]);
+Console.WriteLine("Ссума натуральных чисел равна: " + GetSum(start, end));
+
+int GetSum(int start, int end)
+{
+    if (start == end)
+    {
+        return start;
+    }
+    return start + GetSum(++start, end);
+}
